607fc893b357c8cdb621da201f5780f0bae45572
Cameron Burchnall <cameron.burchnall@ntlworld.com>
2024-12-06 15:22:38 +0000

    Moved nav out of header Added border to header, main and footer Created sizing for header, main and footer Cloned nav text style onto welcome message Adjusted welcome message text Changed all font and background colours to new colour scheme Changed background image Added and formatted main text Added scroll to top button

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-let firstname = prompt("Please Enter Your Name");
+// let firstname = prompt("Please Enter Your Name");
 
-const welcome = document.querySelector("#Welcome");
-welcome.textContent = `Welcome ${firstname}`;
+// const welcome = document.querySelector("#Welcome");
+// welcome.textContent = `Welcome to the jungle ${firstname}`;
+
+const topbutton = document.querySelector("#scroll");
+
+window.addEventListener("scroll", checkheight);
+
+function checkheight() {
+  // window is the current window and scroll is the Y axis scrolled
+  if (window.scrollY > 500) {
+    topbutton.style.display = "flex";
+  } else {
+    topbutton.style.display = null;
+  }
+}
+topbutton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
